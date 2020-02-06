db197b9f118b9d40bdaadaea94696316af9279f4
mauricio mena <mmena@homeaway.com>
2020-02-05 20:55:20 -0700

    Adding node code

---
+++ repository/projectSite/static/js/config.js
@@ -0,0 +1,6 @@
+const cognitoSettings = {
+  UserPoolId: 'us-east-1_qLwRiooaV',
+  ClientId: 'dl4hi9t422qfs2oue9np5rdpo'
+}
+
+const apiGPrefix = 'https://4cgnkbjnkk.execute-api.us-east-1.amazonaws.com/dev/'
