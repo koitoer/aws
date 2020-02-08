8365c2cc12c7b5eed6d2c072b27b0282663d4c94
mauricio mena <mmena@homeaway.com>
2020-02-07 20:13:35 -0700

    add non authorized request

--- repository/projectSite/backend/gql_endpoint.js
+++ repository/projectSite/backend/gql_endpoint.js
@@ -4,14 +4,19 @@ const graphql = require('graphql')
 const schema = require('./lib/schema')
 
 function runQuery (query, claims, variables) {
+  if (claims === null) {
+    return graphql.graphql(schema.PublicSchema, query, {}, null, variables)
+  }
   return graphql.graphql(schema.Schema, query, {claims: claims}, null, variables)
 }
 
 module.exports.handler = (event, context, cb) => {
   console.log('Received event', JSON.stringify(event))
 
-  const userInfo = event.requestContext.authorizer.claims
-  console.log(`Event from user ${userInfo.name} with ID ${userInfo.sub}`)
+  var userInfo = null;
+  if (event.requestContext.authorizer) {
+    userInfo = event.requestContext.authorizer.claims
+  }
 
   const request = JSON.parse(event.body)
   console.log('Query: ' + request.query)
@@ -25,7 +30,6 @@ module.exports.handler = (event, context, cb) => {
         headers: { 'Access-Control-Allow-Origin': '*' },
         body: JSON.stringify(response)
       }
-      console.log('Built response')
       console.log(respified)
       return respified
     })
